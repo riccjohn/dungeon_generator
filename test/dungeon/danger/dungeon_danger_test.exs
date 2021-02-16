2c7ff19f2de6e1578c26a102dcaecd7f31909ef0
riccjohn <riccjohn@gmail.com>
2021-02-16 09:45:23 -0600

    Removes outdated test

--- test/dungeon/danger/dungeon_danger_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule DungeonGeneratorTest.Danger do
-  use ExUnit.Case
-  doctest DungeonGenerator.Danger
-
-  test "generate_danger" do
-    danger = DungeonGenerator.Danger.generate_danger()
-    assert danger
-  end
-end
